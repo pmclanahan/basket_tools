ae05df2e27f078f8582c65b6b8e47ab8d3606dae
Paul McLanahan <pmac@mozilla.com>
2014-11-18 14:44:21 -0500

    Add dedupe of email.

--- get_tokens.py
+++ get_tokens.py
@@ -11,6 +11,7 @@ import basket
 
 
 SUPERTOKEN = os.getenv('SUPERTOKEN')
+DUPE_DESTROYER = []
 
 # Wrap sys.stdout into a StreamWriter to allow writing unicode.
 sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
@@ -22,16 +23,21 @@ def get_new_token():
 
 def get_user(email):
     try:
-        user_data = basket.debug_user(email, SUPERTOKEN)
+        return basket.debug_user(email, SUPERTOKEN)
     except basket.BasketException:
-        user_data = None
-    return user_data
+        return None
 
 
 def get_emails_from_file(filename):
     with codecs.open(filename, encoding='utf8') as f:
         for line in f:
-            yield line.strip()
+            email = line.strip()
+            lc_email = email.lower()
+            if lc_email in DUPE_DESTROYER:
+                continue
+
+            DUPE_DESTROYER.append(lc_email)
+            yield email
 
 
 def any_missmatches(filename):
@@ -45,6 +51,7 @@ if __name__ == '__main__':
 
     print 'Email,Token,In ET,In Basket'
     emailfile = sys.argv[1]
+    count = 0
     for email in get_emails_from_file(emailfile):
         in_basket = False
         in_ET = False
@@ -60,5 +67,8 @@ if __name__ == '__main__':
             token = get_new_token()
 
         print u'%s,%s,%s,%s'.decode('utf8') % (email, token, in_ET, in_basket)
+        count += 1
         sys.stderr.write('.')
         sys.stderr.flush()
+
+    sys.stderr.write('\n\nWrote {0} tokens to stdout.'.format(count))
